4a6b6098277e3ae337e13dc35752140676a15487
gsralex <gsralex@163.com>
2026-09-13 12:19:33 +0800

    [init]: react project

---
+++ rove-console/src/main/java/com/gsralex/rove/console/Main.java
@@ -0,0 +1,12 @@
+package com.gsralex.rove.console;
+
+import com.gsralex.rove.core.Rove;
+
+public final class Main {
+
+    private Main() {}
+
+    public static void main(String[] args) {
+        System.out.println(Rove.greeting());
+    }
+}
---
+++ rove-core/src/main/java/com/gsralex/rove/core/Rove.java
@@ -0,0 +1,10 @@
+package com.gsralex.rove.core;
+
+public final class Rove {
+
+    private Rove() {}
+
+    public static String greeting() {
+        return "Hello from rove-core";
+    }
+}
